f8e29301a0b95e2daa7b8024828870fd4d4d7f38
gayoung <gayoung990911@gmail.com>
2025-06-15 17:07:26 +0900

    fix: 로그인 화면에서 뒤로가기 올바르게 동작하지 않는 문제 수정

--- app/src/main/java/net/ifmain/monologue/app/MainActivity.kt
+++ app/src/main/java/net/ifmain/monologue/app/MainActivity.kt
@@ -8,33 +8,20 @@ import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableLongStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import dagger.hilt.android.AndroidEntryPoint
-import net.ifmain.monologue.ui.screen.DiaryScreen
-import net.ifmain.monologue.ui.screen.DiaryListScreen
-import net.ifmain.monologue.ui.screen.IntroScreen
-import net.ifmain.monologue.ui.screen.auth.SignInScreen
-import net.ifmain.monologue.ui.screen.auth.SignUpScreen
-import net.ifmain.monologue.ui.theme.MonologueTheme
-import net.ifmain.monologue.viewmodel.DiaryViewModel
-import net.ifmain.monologue.viewmodel.IntroViewModel
-import net.ifmain.monologue.viewmodel.SignInViewModel
-import net.ifmain.monologue.viewmodel.SignUpViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.navigation.NavType
 import androidx.navigation.navArgument
-import net.ifmain.monologue.ui.screen.LicenseScreen
-import net.ifmain.monologue.ui.screen.SettingsScreen
+import dagger.hilt.android.AndroidEntryPoint
+import net.ifmain.monologue.ui.screen.*
+import net.ifmain.monologue.ui.screen.auth.*
+import net.ifmain.monologue.ui.theme.MonologueTheme
+import net.ifmain.monologue.viewmodel.*
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -52,8 +39,7 @@ class MainActivity : ComponentActivity() {
 
 @RequiresApi(Build.VERSION_CODES.TIRAMISU)
 @Composable
-fun StartNavigation(
-) {
+fun StartNavigation() {
     val navController = rememberNavController()
     var userId by remember { mutableStateOf<String?>(null) }
     val context = LocalContext.current
@@ -66,8 +52,16 @@ fun StartNavigation(
         composable("intro_screen") {
             val introViewModel: IntroViewModel = hiltViewModel()
             IntroScreen(
-                onSignInClick = { navController.navigate("sign_in_screen") },
-                onSignUpClick = { navController.navigate("sign_up_screen") },
+                onSignInClick = {
+                    navController.navigate("sign_in_screen") {
+                        launchSingleTop = true
+                    }
+                },
+                onSignUpClick = {
+                    navController.navigate("sign_up_screen") {
+                        launchSingleTop = true
+                    }
+                },
                 onNavigateToDiaryScreen = { name, id ->
                     userId = id
                     navController.navigate("diary_write_screen/$userId") {
@@ -85,42 +79,38 @@ fun StartNavigation(
         }
         composable("sign_in_screen") {
             val signInViewModel: SignInViewModel = hiltViewModel()
-
             SignInScreen(
                 signInViewModel = signInViewModel,
                 introViewModel = hiltViewModel(),
                 onNavigateToDiaryScreen = { name, id ->
                     userId = id
                     navController.navigate("diary_write_screen/$userId") {
-                        popUpTo("sign_in_screen") { inclusive = true }
+                        popUpTo("intro_screen") { inclusive = true }
                     }
                 },
                 onNavigateToDiaryList = { name, id ->
                     userId = id
                     navController.navigate("diary_list_screen/$userId") {
-                        popUpTo("sign_in_screen") { inclusive = true }
+                        popUpTo("intro_screen") { inclusive = true }
                     }
                 },
             )
         }
         composable("sign_up_screen") {
             val signUpViewModel: SignUpViewModel = hiltViewModel()
-
             SignUpScreen(
                 viewModel = signUpViewModel,
                 onNavigateToMain = { name, id ->
                     userId = id
                     navController.navigate("diary_write_screen/$userId") {
-                        popUpTo("sign_up_screen") { inclusive = true }
+                        popUpTo("intro_screen") { inclusive = true }
                     }
                 },
             )
         }
         composable(
             route = "diary_write_screen/{userId}",
-            arguments = listOf(navArgument("userId") {
-                type = NavType.StringType
-            })
+            arguments = listOf(navArgument("userId") { type = NavType.StringType })
         ) { backStackEntry ->
             val diaryViewModel: DiaryViewModel = hiltViewModel()
             diaryViewModel.userId = userId ?: ""
@@ -132,12 +122,8 @@ fun StartNavigation(
                 onAnalyzeClick = { diaryViewModel.onAnalyzeClick() },
                 onSaveClick = { selectedMood, textToSave ->
                     diaryViewModel.onSaveClick(
-                        onError = { errorMessage ->
-                            println("Error: $errorMessage")
-                        },
-                        onSuccess = {
-                            println("Successfully saved!")
-                        }
+                        onError = { errorMessage -> println("Error: $errorMessage") },
+                        onSuccess = { println("Successfully saved!") }
                     )
                 },
                 onNavigateToDiaryList = {
@@ -147,37 +133,30 @@ fun StartNavigation(
                 }
             )
         }
-
         composable(
-            "diary_list_screen/{userId}",
-            arguments = listOf(navArgument("userId") {
-                type = NavType.StringType
-            })
+            route = "diary_list_screen/{userId}",
+            arguments = listOf(navArgument("userId") { type = NavType.StringType })
         ) { backStackEntry ->
             val diaryViewModel: DiaryViewModel = hiltViewModel()
             diaryViewModel.userId = userId ?: ""
             val userId = backStackEntry.arguments!!.getString("userId")!!
-
             DiaryListScreen(
                 viewModel = diaryViewModel,
                 userId = userId,
                 onNavigateToDiaryDetail = { entry ->
                     navController.navigate("diary_detail_screen/${entry.date}")
-
                 },
                 onNavigateToSettings = {
                     navController.navigate("settings_screen/$userId")
                 }
             )
         }
-
         composable("diary_detail_screen/{date}") { backStackEntry ->
             val date = backStackEntry.arguments!!.getString("date")!!
             val diaryViewModel: DiaryViewModel = hiltViewModel()
             diaryViewModel.userId = userId ?: ""
             val entries by diaryViewModel.diaryEntries.collectAsStateWithLifecycle(initialValue = emptyList())
             val diaryEntry = entries.firstOrNull { it.date == date }
-
             DiaryScreen(
                 userId = diaryViewModel.userId,
                 diaryEntry = diaryEntry,
@@ -192,12 +171,9 @@ fun StartNavigation(
                 onNavigateToDiaryList = { navController.popBackStack() }
             )
         }
-
         composable(
             route = "settings_screen/{userId}",
-            arguments = listOf(navArgument("userId") {
-                type = NavType.StringType
-            })
+            arguments = listOf(navArgument("userId") { type = NavType.StringType })
         ) { backStackEntry ->
             SettingsScreen(
                 onNavigateToIntro = {
@@ -205,35 +181,46 @@ fun StartNavigation(
                         popUpTo(0) { inclusive = true }
                     }
                 },
-                onNavigateToLicense = { navController.navigate("license_screen") }
+                onNavigateToLicense = {
+                    navController.navigate("license_screen")
+                }
             )
         }
-
         composable("license_screen") {
             LicenseScreen()
         }
     }
 
     BackHandler {
-        val currentRoute = navController.currentBackStackEntry?.destination?.route
-
+        val currentRoute = navController.currentBackStackEntry?.destination?.route ?: ""
         val now = System.currentTimeMillis()
 
-        val exitRoutes = listOf(
-            "intro_screen",
-            "diary_write_screen",
-            "sign_in_screen",
-            "sign_up_screen"
-        )
-
         when {
-            currentRoute == "sign_in_screen" || currentRoute == "sign_up_screen" -> {
-                navController.popBackStack("intro_screen", false)
+            currentRoute.startsWith("sign_in_screen") || currentRoute.startsWith("sign_up_screen") -> {
+                navController.navigate("intro_screen") {
+                    popUpTo("sign_in_screen") { inclusive = true }
+                }
             }
-
+            currentRoute.startsWith("license_screen") -> {
+                navController.popBackStack("settings_screen/${userId}", false)
+            }
+            currentRoute.startsWith("settings_screen") -> {
+                navController.popBackStack("diary_list_screen/${userId}", false)
+            }
+            currentRoute.startsWith("intro_screen") || currentRoute.startsWith("diary_list_screen") -> {
+                if (now - backPressedTime < 2000) {
+                    (context as? ComponentActivity)?.finish()
+                } else {
+                    Toast.makeText(
+                        context,
+                        "뒤로가기를 한 번 더 누르면 앱이 종료됩니다.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    backPressedTime = now
+                }
+            }
             else -> {
-                val popped = navController.popBackStack()
-                if (!popped || currentRoute in exitRoutes) {
+                if (!navController.popBackStack()) {
                     if (now - backPressedTime < 2000) {
                         (context as? ComponentActivity)?.finish()
                     } else {
